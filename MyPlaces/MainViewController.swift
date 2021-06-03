a51234115a40f68c15da62d719eaa3a89549aa89
Beibarys7 <beibarys1927@gmail.com>
2021-06-03 16:45:29 +0600

    Initial commit

---
+++ MyPlaces/MainViewController.swift
@@ -0,0 +1,41 @@
+//
+//  MainViewController.swift
+//  MyPlaces
+//
+//  Created by Beibarys Shaggy on 03.06.2021.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+
+    let resNames = ["Bonsai","Burger Heroes","Kitchen","Love&Life","Morris Pub","Sherlock Holmes","Speak Easy","X.O"]
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+  
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return resNames.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = resNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: resNames[indexPath.row])
+        
+        return cell
+    }
+    
+
+}
